3bd6a35c281b0d515092f95ddd6239b7fa262f49
W3418 <W3418@lab.aca>
2020-01-08 18:35:36 -0500

    He agregado un pequeño detalle

--- programa.ts
+++ programa.ts
@@ -1,6 +1,11 @@
 let num1 : number = 1
 let num2 : number = 1
 
+let terminado : boolean = false
+terminado = true
+
+let nombre : string = "Pips"
+
 let suma = num1 + num2
 
 console.log(`La suma es : ${suma}`)
