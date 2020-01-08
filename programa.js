e79a45d538faa85ebdfe36d0501c324ff7c58880
W3418 <W3418@lab.aca>
2020-01-08 18:34:07 -0500

    He creado el proyecto

---
+++ programa.js
@@ -0,0 +1,4 @@
+var num1 = 1;
+var num2 = 1;
+var suma = num1 + num2;
+console.log("La suma es : " + suma);
---
+++ programa.ts
@@ -0,0 +1,6 @@
+let num1 : number = 1
+let num2 : number = 1
+
+let suma = num1 + num2
+
+console.log(`La suma es : ${suma}`)
